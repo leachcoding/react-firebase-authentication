f9c97cb0f58f897b3e3ff8c5b0255b4124f7caae
ndurbin <nick.durbin@gmail.com>
2019-07-03 11:35:54 -0400

    Pushing the changes.

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+import FooterItem from '../Footer/Footer';
 
 const Landing = () => (
-  <div>
-    <h1>Landing</h1>
+  <Router>
+   <div>
+    
+      <Switch>
+        <Route path="/" exact component={About} /> 
+        <Route path="/contact" component={Contact} />
+      </Switch>
+      <FooterItem />
   </div>
+  </Router>
 );
 
 export default Landing;
