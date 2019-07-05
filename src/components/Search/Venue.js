d3e25cc752edc2c141dcf040d1e098b3a4a94cde
bobbidigi <bobbidigi34@gmail.com>
2019-07-05 01:43:31 -0400

    css and display of venues outside of hero image.

--- src/components/Search/Venue.js
+++ src/components/Search/Venue.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export const Venue = ({ name, location, city, state }) => (
     
         <li className='venue'>
-            <a  href="#">{name}</a>
+            <a href="#">{name}</a>
             <p>{location}</p>
             <p>{city}</p>
             <p>{state}</p>
